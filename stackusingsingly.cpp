75cf59a2e8198bb0e31fdd42e2abf053dce99881
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-10 14:02:37 +0700

    Menambahkan method isEmpty di dalam class stack

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -55,4 +55,10 @@ class Stack {
             cout << endl;
         }// return the value of the top node
     }
+
+    // IsEmpty operation: check if the stack is empty
+    bool isEmpty() {
+        return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+    }
+    
 };
