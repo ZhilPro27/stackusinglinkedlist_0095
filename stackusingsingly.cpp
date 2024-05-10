750a404fd73469d94ecd39c092dfcd1735e895e6
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-10 13:55:43 +0700

    Menambahkan method pop di dalam class stack

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -37,5 +37,7 @@ class Stack {
         if (isEmpty()) {
             cout << "Stack is empty" << endl;
         }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; // Update the top pointer to the next node
     }
 };
