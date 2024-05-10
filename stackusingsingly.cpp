fa3ba20ce0d70288794cea8432479a75ae8290c3
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-10 13:49:49 +0700

    Menambahkan pengecekan stack kosong atau tidak di dalam method pop pada class stack

--- stackusingsingly.cpp
+++ stackusingsingly.cpp
@@ -31,4 +31,11 @@ class Stack {
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    // Pop operation: Remove the topmost element from the stack
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+        }
+    }
 };
